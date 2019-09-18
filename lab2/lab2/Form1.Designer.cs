7abb4b31b4628d93e76cc4fb7a8715e2f7d4a307
Alexey Rahuba <alexeyrahuba@yandex.ru>
2019-09-18 20:15:52 +0300

    grayscale transform is done

---
+++ lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(388, 389);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(406, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(388, 389);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Оттенки серого1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(652, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Разность";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(406, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Оттенки серого2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Оттенки серого";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lab2/lab2/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{ 
+    public partial class Form1 : Form
+    {
+        PictureBox currentPB;
+
+        public Form1()
+        {
+            InitializeComponent();
+            currentPB = pictureBox1;
+        }  
+
+        private void load_image()
+        {
+            Bitmap image;
+
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if (open_dialog.ShowDialog() == DialogResult.OK) 
+            {
+                try
+                {
+                    image = new Bitmap(open_dialog.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox1.Image = image;
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            pictureBox2.Image = (Image)pictureBox1.Image.Clone();
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            load_image();
+        }
+
+        private void grayscale_transform(bool button)
+        {
+            if (button)
+                currentPB = pictureBox1;
+            else
+                currentPB = pictureBox2;
+
+            if (currentPB.Image != null)
+            {
+                Bitmap bmp = currentPB.Image as Bitmap;
+
+                // Lock the bitmap's bits.  
+                Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                System.Drawing.Imaging.BitmapData bmpData =
+                    bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                    bmp.PixelFormat);
+
+                // Get the address of the first line.
+                IntPtr ptr = bmpData.Scan0;
+
+                // Declare an array to hold the bytes of the bitmap.
+                int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+                byte[] rgbValues = new byte[bytes];
+
+                // Copy the RGB values into the array.
+                System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+                //int i = 0;
+                for (int counter = 0; counter < rgbValues.Length; counter += 3)
+                {
+                    int gray;
+                    if (button)
+                        gray = Convert.ToInt32(
+                            0.299 * rgbValues[counter + 0] +
+                            0.587 * rgbValues[counter + 1] +
+                            0.114 * rgbValues[counter + 2]);
+                    else
+                        gray = Convert.ToInt32(
+                            0.0722 * rgbValues[counter + 0] +
+                            0.7152 * rgbValues[counter + 1] +
+                            0.2126 * rgbValues[counter + 2]);
+                    rgbValues[counter + 0] = (byte)gray;
+                    rgbValues[counter + 1] = (byte)gray;
+                    rgbValues[counter + 2] = (byte)gray;           
+
+                    /*bmp.SetPixel(i % bmp.Width, i / bmp.Width,
+                        Color.FromArgb(
+                            (Int32)(0.299*rgbValues[counter]), 
+                            (Int32)(0.587*rgbValues[counter + 1]), 
+                            (Int32)(0.114*rgbValues[counter + 2]))
+                        );
+                    i++;*/
+
+                }
+                // Copy the RGB values back to the bitmap
+                System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+                // Unlock the bits.
+                bmp.UnlockBits(bmpData);
+                currentPB.Refresh();
+            }
+        }
+         
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            grayscale_transform(true);   
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            grayscale_transform(false);
+        }
+
+
+    }
+}
