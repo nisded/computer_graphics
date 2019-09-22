9037497758fbdbb1d615344119ae199a5b05163b
Alexey Rahuba <alexeyrahuba@yandex.ru>
2019-09-22 23:37:58 +0300

    flood fill is done

---
+++ lab3/raster_algorithms/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace raster_algorithms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(655, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(766, 142);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(38, 38);
+            this.panel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(674, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выбор цвета заливки";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(674, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(108, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Заливка цветом";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 450);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ lab3/raster_algorithms/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace raster_algorithms
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Color fillColor;
+        Bitmap bmp;
+
+        public Form1()
+        {           
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(pictureBox1.Image);
+            fillColor = Color.Black;
+
+        }        
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDial = new ColorDialog();
+            if (colorDial.ShowDialog() == DialogResult.OK)
+            {
+                panel1.BackColor = colorDial.Color;
+                fillColor = colorDial.Color;
+            }
+        }
+
+        private Color get_point_color(Point p)
+        {
+            if (pictureBox1.ClientRectangle.Contains(p))
+                return ((Bitmap)pictureBox1.Image).GetPixel(p.X, p.Y);
+            else
+                return Color.Black;
+        }
+
+        void flood_fill(Point p)
+        {
+            Color curColor = get_point_color(p);
+            Color oldColor = curColor;
+            Point leftP = p;
+            Point rightP = p;
+            if (!pictureBox1.ClientRectangle.Contains(p))
+                return;
+            if (curColor != fillColor && curColor == oldColor)
+            {
+                while (curColor == oldColor && pictureBox1.ClientRectangle.Contains(p))
+                {
+                    leftP.X -= 1;
+                    curColor = get_point_color(leftP);
+                }
+                leftP.X += 1;
+                curColor = get_point_color(p);
+                while (curColor == oldColor && pictureBox1.ClientRectangle.Contains(p))
+                {
+                    rightP.X += 1;
+                    curColor = get_point_color(rightP);
+                }
+                rightP.X -= 1;
+                Pen pen = new Pen(fillColor, 1);
+                g.DrawLine(pen, leftP, rightP);
+
+                for (int i = leftP.X; i <= rightP.X; ++i)
+                {
+                    Point pAbove = new Point(i, p.Y + 1);
+                    Color curC = get_point_color(pAbove);
+                    if (curC.ToArgb() != fillColor.ToArgb() && pictureBox1.ClientRectangle.Contains(pAbove)) 
+                        flood_fill(pAbove);
+                }
+                for (int i = leftP.X; i <= rightP.X; ++i)
+                {
+                    Point pBelow = new Point(i, p.Y - 1);
+                    Color curC = get_point_color(pBelow);
+                    if (curC.ToArgb() != fillColor.ToArgb() && pictureBox1.ClientRectangle.Contains(pBelow))
+                        flood_fill(pBelow);
+                }
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) {
+                MouseEventArgs m = (MouseEventArgs)e;
+                Point p = m.Location;
+                flood_fill(p);
+            }
+            pictureBox1.Invalidate();
+        }
+    }
+}
