4e6a012cfa8fafb99103dad1ed0363d7408f99e8
ITyuri <pishkov97@yandex.ru>
2019-09-26 20:41:26 +0300

    Added: selection border.

--- lab3/raster_algorithms/Form1.cs
+++ lab3/raster_algorithms/Form1.cs
@@ -17,13 +17,19 @@ namespace raster_algorithms
         Bitmap bmp;
         Boolean isMouseDown;
         Point prevPoint;
+        TextureBrush textureBrush;
+
+        Point mouseCoord;
 
         public Form1()
         {           
             InitializeComponent();
+
             bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             pictureBox1.Image = bmp;
             g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+
             fillColor = panel1.BackColor = Color.Blue;
             isMouseDown = false;
             prevPoint = Point.Empty;
@@ -97,16 +103,19 @@ namespace raster_algorithms
                 Point p = m.Location;
                 flood_fill(p);
             }
+            if (radioButton3.Checked)
+            {
+                selectBorder(Color.Red);
+            }
             pictureBox1.Invalidate();
         }
 
         private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            if (!radioButton2.Checked)
-                return;
-            isMouseDown = true;
+            if (radioButton2.Checked)
+                isMouseDown = true;
             prevPoint = e.Location;
-
+            mouseCoord = e.Location;
         }
 
         private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
@@ -124,12 +133,125 @@ namespace raster_algorithms
         {
             isMouseDown = false;
             prevPoint = Point.Empty;
+            mouseCoord = Point.Empty;
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            g.Clear(Color.Transparent);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
             pictureBox1.Invalidate();
+        }
+
+        private void loadFillImage()
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter =
+                "Image Files(*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Image img = Image.FromFile(openDialog.FileName);
+                    pictureBox2.Image = (Image)img.Clone();
+                    pictureBox1.Image = (Image)img.Clone();
+                    bmp = new Bitmap(pictureBox1.Image);
+                    pictureBox1.Image = bmp;
+                    textureBrush = new TextureBrush(img);
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void chooseImageBtn_Click(object sender, EventArgs e)
+        {
+            loadFillImage();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+                pictureBox1.Cursor = Cursors.Cross;
+            else
+                pictureBox1.Cursor = Cursors.Default;
+        }
+
+        // найти точку, принадлежащую границе
+        private Point findStartPoint()
+        {
+            int x = mouseCoord.X;
+            int y = mouseCoord.Y;
+
+            Color bgColor = bmp.GetPixel(mouseCoord.X, mouseCoord.Y);
+            Color currColor = bgColor;
+            while (x < bmp.Width - 2 && currColor.ToArgb() == bgColor.ToArgb())
+            {
+                x++;
+                currColor = bmp.GetPixel(x, y);
+            }
+
+            return new Point(x, y);
+        }
+
+        // выделить границу
+        private void selectBorder(Color c)
+        {
+            List<Point> pixels = new List<Point>();
+            Point curr = findStartPoint();
+            Point start = curr;
+            pixels.Add(start);
+            Color borderColor = bmp.GetPixel(curr.X, curr.Y);
+
+            Point next = new Point();
+            int currDir = 6;
+            int nextDir = -1;
+            int moveTo = 0;
+            // определяем направление движения
+            do
+            {
+                // двигаемся в выбранном направлении
+                moveTo = (currDir - 2 + 8) % 8;
+                int mt = moveTo;
+                do
+                {
+                    next = curr;
+                    switch (moveTo)
+                    {
+                        case 0: next.X++; nextDir = 0; break;
+                        case 1: next.X++; next.Y--; nextDir = 1; break;
+                        case 2: next.Y--; nextDir = 2; break;
+                        case 3: next.X--; next.Y--; nextDir = 3; break;
+                        case 4: next.X--; nextDir = 4; break;
+                        case 5: next.X--; next.Y++; nextDir = 5; break;
+                        case 6: next.Y++; nextDir = 6; break;
+                        case 7: next.X++; next.Y++; nextDir = 7; break;
+                    }
+
+                    if (next == start)
+                        break;
+
+                    if (bmp.GetPixel(next.X, next.Y) == borderColor)
+                    {
+                        pixels.Add(next);
+                        curr = next;
+                        currDir = nextDir;
+                        break;
+                    }
+                    moveTo = (moveTo + 1) % 8;
+                } while (moveTo != mt);
+            } while (next != start);
+
+            // меняем цвет грацицы
+            foreach (var p in pixels)
+                bmp.SetPixel(p.X, p.Y, c);
         }
     }
 }
+
+// Ограничить возможность рисования границы
+
+// Выделение границы после удаления текстуры
