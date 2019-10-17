f910b4337a16e67b396f7fde6c5cfcb96ac7a95e
ITyuri <pishkov97@yandex.ru>
2019-10-17 21:13:34 +0300

    LSystems

---
+++ lab5/LSytems/LSytems/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace LSytems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.inputControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.createBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.inputControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(502, 371);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // inputControl
+            // 
+            this.inputControl.Controls.Add(this.tabPage1);
+            this.inputControl.Controls.Add(this.tabPage2);
+            this.inputControl.Location = new System.Drawing.Point(0, 451);
+            this.inputControl.Name = "inputControl";
+            this.inputControl.Padding = new System.Drawing.Point(20, 3);
+            this.inputControl.SelectedIndex = 0;
+            this.inputControl.Size = new System.Drawing.Size(526, 148);
+            this.inputControl.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.inputBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(518, 122);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Input";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputBox.Location = new System.Drawing.Point(3, 3);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(512, 116);
+            this.inputBox.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.outputBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(858, 122);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Output";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputBox.Location = new System.Drawing.Point(3, 3);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(852, 116);
+            this.outputBox.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(526, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Iterations:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(71, 36);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // createBtn
+            // 
+            this.createBtn.Location = new System.Drawing.Point(129, 34);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(96, 23);
+            this.createBtn.TabIndex = 5;
+            this.createBtn.Text = "Create";
+            this.createBtn.UseVisualStyleBackColor = true;
+            this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 599);
+            this.Controls.Add(this.createBtn);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputControl);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "L-System";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.inputControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl inputControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button createBtn;
+    }
+}
+
---
+++ lab5/LSytems/LSytems/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LSytems
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        string axiom;
+        double angle;
+        string direction;
+        int iterations;
+        SortedDictionary<char, string> rules;
+        Stack<Tuple<double, double, double, double>> savedStates;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            rules = new SortedDictionary<char, string>();
+            savedStates = new Stack<Tuple<double, double, double, double>>();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Text files|*.TXT";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fname = openDialog.FileName;
+                    string[] flines = File.ReadAllLines(fname);
+                    string[] parameters = flines[0].Split(' ');
+
+                    axiom = parameters[0];
+                    angle = Convert.ToDouble(parameters[1]);
+                    direction = parameters[2];
+
+                    rules.Clear();
+                    inputBox.Text = "";
+                    string[] rule;
+                    inputBox.AppendText(flines[0] + "\r\n");
+                    for (int i = 1; i < flines.Length; ++i)
+                    {
+                        inputBox.AppendText(flines[i] + "\r\n");
+                        rule = flines[i].Split('>');
+                        rules[Convert.ToChar(rule[0])] = rule[1];
+                    }
+                }
+                catch
+                {
+                    DialogResult result = MessageBox.Show("Can't open file",
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        string buildPath()
+        {
+            string prev = axiom;
+            string next = axiom;
+            int iter = 0;
+            while (iter < iterations)
+            {
+                prev = next;
+                next = "";
+                for (int i = 0; i < prev.Length; ++i)
+                {
+                    if (rules.ContainsKey(prev[i]))
+                        next += rules[prev[i]];
+                    else
+                        next += prev[i];
+                }
+                ++iter;
+            }
+            return next;
+        }
+
+        void drawLSystem(string path)
+        {
+            List<double> xPoints = new List<double>();
+            List<double> yPoints = new List<double>();
+            List<Tuple<double, double, double, double>> lSystPoints = 
+                new List<Tuple<double, double, double, double>>();
+            double x = 0, y = 0, dx = 0, dy = 0;
+
+            switch (direction)
+            {
+                case "LEFT":
+                    x = pictureBox1.Width;
+                    y = pictureBox1.Height / 2;
+                    dx = -(pictureBox1.Width / Math.Pow(10, iterations + 1));
+                    break;
+
+                case "RIGHT":
+                    y = pictureBox1.Height / 2;
+                    dx = pictureBox1.Width / Math.Pow(10, iterations + 1);
+                    break;
+
+                case "UP":
+                    x = pictureBox1.Width / 2;
+                    y = pictureBox1.Height;
+                    dy = -(pictureBox1.Height / Math.Pow(10, iterations + 1));
+                    break;
+
+                case "DOWN":
+                    x = pictureBox1.Width / 2;
+                    dy = pictureBox1.Height / Math.Pow(10, iterations + 1);
+                    break;
+
+                default: break;
+            }
+
+            xPoints.Add(x);
+            yPoints.Add(y);
+
+            double rx, ry;
+            for (int i = 0; i < path.Length; ++i)
+            {
+                switch (path[i])
+                {
+                    case 'F':
+                        lSystPoints.Add(
+                            new Tuple<double, double, double, double>(x, y, x + dx, y + dy));
+                        x += dx;
+                        y += dy;
+                        xPoints.Add(x);
+                        yPoints.Add(y);
+                        break;
+
+                    case '+':
+                        rx = dx;
+                        ry = dy;
+                        dx = rx * Math.Cos(angle * Math.PI / 180) - ry * Math.Sin(angle * Math.PI / 180);
+                        dy = rx * Math.Sin(angle * Math.PI / 180) + ry * Math.Cos(angle * Math.PI / 180);
+                        break;
+
+                    case '-':
+                        rx = dx;
+                        ry = dy;
+                        dx = rx * Math.Cos(-angle * Math.PI / 180) - ry * Math.Sin(-angle * Math.PI / 180);
+                        dy = rx * Math.Sin(-angle * Math.PI / 180) + ry * Math.Cos(-angle * Math.PI / 180);
+                        break;
+
+                    case '[':
+                        savedStates.Push(new Tuple<double, double, double, double>(x, y, dx, dy));
+                        break;
+
+                    case ']':
+                        Tuple<double, double, double, double> coords = savedStates.Pop();
+                        x = coords.Item1;
+                        y = coords.Item2;
+                        dx = coords.Item3;
+                        dy = coords.Item4;
+                        break;
+
+                    default: break;
+                }
+            }
+
+            double xMax = xPoints.Max();
+            double xMin = xPoints.Min();
+            double yMax = yPoints.Max();
+            double yMin = yPoints.Min();
+            double scale = Math.Max(xMax - xMin, yMax - yMin);
+
+            foreach (var ps in lSystPoints)
+                g.DrawLine(Pens.Blue,
+                    (float)((xMax - ps.Item1) / scale * pictureBox1.Width),
+                    (float)((yMax - ps.Item2) / scale * pictureBox1.Height),
+                    (float)((xMax - ps.Item3) / scale * pictureBox1.Width),
+                    (float)((yMax - ps.Item4) / scale * pictureBox1.Height));
+        }
+
+        private void createBtn_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            outputBox.Text = "";
+            iterations = Convert.ToInt32(numericUpDown1.Value);
+            string path = buildPath();
+            drawLSystem(path);
+            outputBox.Text = path;
+            pictureBox1.Invalidate();
+        }
+    }
+}
